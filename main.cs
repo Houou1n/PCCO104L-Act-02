2dd9bd0cb9754583aae4c1e11f96d729f0984192
Houou1n <158658886+Houou1n@users.noreply.github.com>
2024-02-21 12:38:56 +0000

    final

--- main.cs
+++ main.cs
@@ -2,5 +2,25 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
+
+    for (int i = 1; i < 101; i++)
+    { 
+      if (i % 3 == 0 && i % 5 > 0)
+      {
+        Console.WriteLine(i + " - Foo");
+      }
+      else if (i % 3 > 0 && i % 5 == 0)
+      {
+        Console.WriteLine(i + " - Bar");
+      }
+      else if (i % 3 == 0 && i % 5 == 0)
+      {
+        Console.WriteLine(i + "- Foobar");
+      }
+      else if (i % 3 > 0 && i % 5 > 0)
+      {
+        Console.WriteLine(i);
+      }
+    }
   }
 }
